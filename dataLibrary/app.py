d3fbeb52eaee889ab234983415910275f7062d95
s4t0masami <sadbuenocat@gmail.com>
2024-12-08 00:47:08 -0300

    fourth commit

--- dataLibrary/app.py
+++ dataLibrary/app.py
@@ -7,7 +7,7 @@ dataBase = {}
 root = Tk()
 
 def settingRoot():
-    root.geometry("1000x1000")
+    root.geometry("1080x780")
     root.title("JSON with Tkinter")
 
 def verifyDataFile():
@@ -16,27 +16,72 @@ def verifyDataFile():
         with open("dataLibrary/data.json", "w") as f:
             json.dump(dataBase, f, indent=4)
             labelData.config(text="no data get")
+            labelKeys.config(text='no keys')
+            labelValues.config(text='no values')
     else:
         with open("dataLibrary/data.json", "r") as f:
             dataGet = json.load(f)
-            dataGet = str(dataGet).replace("{", "").replace("}", "")
-            labelData.config(text=dataGet)
-            return dataGet
+
+
+            keys = list(dataGet.keys())
+            values = list(dataGet.values())
+    
+            dataShow = '\n'.join(list(keys) + list(values))
+            labelData.config(text=dataShow)
+
+            labelKeys.config(text='\n'.join(keys))
+            labelValues.config(text='\n'.join(values))
+
+def sendData():
+    value = entryValue.get().strip()
+    key = entryKey.get().strip()
+    verifyDataFile()
+    
+    if key and value:
+        dataGet[key] = value
+        with open("dataLibrary/data.json", "w") as f:
+            json.dump(dataGet, f, indent=4) 
+        verifyDataFile()
+        buttonSend.config(bg=None)
+
+    else:
+        buttonSend.config(bg='red')
 
 settingRoot()
+# Row 0
 
-labelTop = tk.Label(root, text="DATA LIBRARY", font=("Arial", 20, "bold"))
-labelTop.grid(row=0, columnspan=2, padx=20, pady=20)
+labelTop = tk.Label(root, text="DATA LIBRARY", font=("Arial", 30, "bold"))
+labelTop.grid(row=0, columnspan=2,pady=20)
+
+#Row 1
+
+labelInfoKeys = tk.Label(root, text="KEYS", font=("Arial",20, "bold")) # label saying keys
+labelInfoKeys.grid(row=1,column=5)
+
+labelInfoValues = tk.Label(root, text="VALUES", font=("Arial",20, "bold")) # label saying values
+labelInfoValues.grid(row=1,column=4)
+
+# Row 2
+
+labelKeys = tk.Label(root, text='', font=("Arial",15)) # label with keys
+labelKeys.grid(row=2,column=5,padx=200)
+
+labelValues = tk.Label(root, text='', font=("Arial",15)) # label with values
+labelValues.grid(row=2,column=4, padx=0)
+
+entryKey = tk.Entry(root, width=30)
+entryKey.grid(row=1,column=1)
+
+entryValue = tk.Entry(root,width=30)
+entryValue.grid(row=1,column=2)
+
+buttonSend = tk.Button(root, text="SEND", command=sendData)
+buttonSend.grid(row=5, columnspan=2, pady=20)
 
 labelData = tk.Label(root, text='', font=("Arial", 20))
-labelData.grid(row=1, columnspan=2, padx=20, pady=20)
+labelData.grid(row=6, columnspan=2, padx=20, pady=20)
 
 verifyDataFile()
-
-keys = dataBase.keys()
-
-label = tk.Label(root, text=keys)
-label.grid(row=2, columnspan=2, padx=20, pady=20)
 
 os.system('cls')
 root.mainloop()
